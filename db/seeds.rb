86ae54324d942544a76ec3a1f55ebd7a01869956
Chase Jensen <soccerchase@gmail.com>
2018-11-16 12:24:38 -0700

    Implemented show functionality for portfolio module

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "5 skills created"
 9.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio_title: #{portfolio_item}",
-    subtitle: "My greate service",
+    subtitle: "My great service",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: 'http://via.placeholder.com/600x400',
     thumb_image: 'http://via.placeholder.com/300x150'
