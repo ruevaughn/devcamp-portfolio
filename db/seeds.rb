68d461059230b42069508690d5fd2b3f28d02065
Chase Jensen <soccerchase@gmail.com>
2018-11-19 14:09:03 -0700

    Update seeds file and add Topics to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,20 @@
-10.times do |blog|
-  Blog.create!(
-   title: "My Blog Post #{blog}",
-   body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
   )
 end
 
-puts "10 blogs posts created"
+puts "3 Topics created"
+
+8.times do |blog|
+  Blog.create!(
+   title: "My Blog Post #{blog}",
+   body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+   topic: Topic.last
+  )
+end
+
+puts "10 Blogs Posts created"
 
 5.times do |skill|
   Skill.create!(
@@ -14,16 +23,19 @@ puts "10 blogs posts created"
   )
 end
 
-puts "5 skills created"
+puts "5 Skills created"
 
-9.times do |portfolio_item|
+portfolio_titles = ['Ruby on Rails', 'Angular']
+portfolio_items = portfolio_titles.count
+portfolio_items.times do |portfolio_item|
+  subject = portfolio_titles[portfolio_item]
   Portfolio.create!(
-    title: "Portfolio_title: #{portfolio_item}",
-    subtitle: "My great service",
+    title: "#{subject}: #{portfolio_item}",
+    subtitle: "#{subject}",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: 'http://via.placeholder.com/600x400',
-    thumb_image: 'http://via.placeholder.com/300x150'
+    thumb_image: 'http://via.placeholder.com/350x200'
   )
 end
 
-puts "9 portfolio items created"
+puts "#{portfolio_items} Portfolio Items created"
