27fa5a105510a19186a8fbf325962382c63123d1
Chase Jensen <soccerchase@gmail.com>
2018-11-19 11:07:10 -0700

    Implement friendly_id on blog model

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -36,7 +36,7 @@ class BlogsController < ApplicationController
   def update
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html { redirect_to @blog, notice: 'Blog Post was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -47,14 +47,14 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'Blog post was successfully destroyed.' }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
-      @blog = Blog.find(params[:id])
+      @blog = Blog.friendly.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
