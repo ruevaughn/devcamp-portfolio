e7fbe1a46b36698661b2e93d17be464526fcefa1
Chase Jensen <soccerchase@gmail.com>
2018-11-19 10:27:41 -0700

    Custom routes for pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :portfolios
   resources :blogs
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
