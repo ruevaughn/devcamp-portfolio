ee4c65dd23f584b2158ff644766e8c5a1b54e86f
Chase Jensen <soccerchase@gmail.com>
2018-11-21 12:53:42 -0700

    Customized devise routesa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   get 'angular-items', to: 'portfolios#angular'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
