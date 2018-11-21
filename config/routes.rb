3fe51c6e9d2ea884eb37ee43d6cd673f2baaa775
Chase Jensen <soccerchase@gmail.com>
2018-11-21 12:08:44 -0700

    Devise model for users and update schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  devise_for :users
   get 'angular-items', to: 'portfolios#angular'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
