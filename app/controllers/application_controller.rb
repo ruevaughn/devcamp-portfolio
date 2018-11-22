641ee5b26c269a391276b827de2157e2050359f6
Chase Jensen <soccerchase@gmail.com>
2018-11-22 08:34:52 -0700

    Use params to set session to control social media tracker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
-
-  # Leaving here because I haven't done this before / in application_controller before.
-  # before_action :configure_permitted_parameters, if: :devise_controller?
-
   include DeviseWhitelist
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name] )
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name] )
-  # end
+  before_action :set_source
 
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
