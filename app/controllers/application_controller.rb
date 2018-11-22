0eb4c477b86f819d32f0898729434ba20d5c9789
Chase Jensen <soccerchase@gmail.com>
2018-11-22 15:08:27 -0700

    Refactor portfolio to use strong parameters properly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,4 @@ class ApplicationController < ActionController::Base
 
   before_action :set_source
 
-
 end
--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -13,8 +13,7 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
-      technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -31,9 +30,8 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
       else
         format.html { render :edit }
@@ -56,6 +54,13 @@ class PortfoliosController < ApplicationController
 
   private
 
-    def portfolio_item_params
+    def portfolio_params
+      params.require(:portfolio).permit(
+        :title,
+        :subtitle,
+        :body,
+        technologies_attributes:
+          [:name]
+        )
     end
 end
